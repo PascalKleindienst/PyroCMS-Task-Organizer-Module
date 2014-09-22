434164efa8be458d6c0dbe502ff5d2fa7cf5f765
Tony G. Bolaño <tonybolanyo@gmail.com>
2014-09-22 20:59:49 +0200

    Added spanish translation

---
+++ language/spanish/permission_lang.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * Task Organizer Permission Language File - ENGLISH
+ * 
+ * @package    PyroCMS
+ * @subpackage Modules
+ * @author     Pascal Kleindienst
+ * @copyright  Copyright (c) 2013, Pascal Kleindienst
+ * @link       http://www.pascalkleindienst.de
+ * @license    LGPLv3
+ */
+// general
+$lang['task_organizer:role_view_all'] = 'Ver todas las tareas';
